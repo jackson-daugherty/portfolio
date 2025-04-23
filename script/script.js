f84045a98fe1b828644cb150beefb6d2475803b4
jackson-daugherty <jackrileydaugherty@gmail.com>
2025-04-23 09:47:11 -0400

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,15 @@
+var coll = document.getElementsByClassName("hamburger_button");
+var i;
+
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.previousElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
